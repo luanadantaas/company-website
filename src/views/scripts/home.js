cd86eeee93cc90a59bbbeb3b09b685e4523dc7f0
marcomansomm <marco.santos@tascomtecnologia.com.br>
2024-05-06 19:30:20 -0300

    refact: all project

--- src/views/scripts/home.js
+++ src/views/scripts/home.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
     $('#myCarousel').carousel();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const loggedIn = user.login;
+    let navbar = document.querySelector('.navbar-nav');
+    
+    if (loggedIn) {
+        let newItem = document.createElement('li');
+        newItem.className = 'nav-item';
+
+        let newLink = document.createElement('a');
+        newLink.className = 'btn btn-primary';
+        newLink.href = '../../views/pages/itSupport.html';
+        newLink.textContent = 'Contact IT';
+
+        newItem.appendChild(newLink);
+
+        navbar.appendChild(newItem);
+    }
+
+    user.origin = "home";
+    localStorage.setItem('user', JSON.stringify(user));
+});
